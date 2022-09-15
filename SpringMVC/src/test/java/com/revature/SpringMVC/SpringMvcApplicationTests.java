68a66608645523473a244571a9682b483f330b93
ryan-schlientz <omega101_ryan@yahoo.com>
2022-09-15 16:11:56 -0400

    Update SpringMvcApplicationTests.java

--- SpringMVC/src/test/java/com/revature/SpringMVC/SpringMvcApplicationTests.java
+++ SpringMVC/src/test/java/com/revature/SpringMVC/SpringMvcApplicationTests.java
@@ -3,7 +3,11 @@ package com.revature.SpringMVC;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+  webEnvironment = WebEnvironment.DEFINED_PORT,
+  properties = {
+    "server.port=8082"
+  })
 class SpringMvcApplicationTests {
 
 	@Test
